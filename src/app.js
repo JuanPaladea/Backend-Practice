bef9d9b2cbf25f7c5ccd5d5c958730821405f803
JuanPaladea <juanpaladea5@gmail.com>
2024-04-02 16:27:47 -0300

    mongoose setup

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import productsRouter from "./routes/products.router.js"
 import { Server } from "socket.io";
 import { __dirname } from './utils.js';
 import ProductManager from './utils/productManager.js';
+import mongoose from 'mongoose';
 
 const app = express();
 const productManagerInstance = new ProductManager("data/products.json")
@@ -16,6 +17,16 @@ app.use(express.static(`${__dirname}/../public`));
 app.engine("handlebars", handlebars.engine());
 app.set("views",`${__dirname}/views`);
 app.set("view engine", "handlebars");
+
+//MONGOOSE
+
+const connection = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017", {dbName: "products"})
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 //BIENVENIDA
 app.get('/', (req, res) => {
