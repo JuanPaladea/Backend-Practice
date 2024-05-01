66a9f98b40972962f3dd589b4b15d184461bb45e
JuanPaladea <juanpaladea5@gmail.com>
2024-05-01 19:53:01 -0300

    jwt passportconfig

--- src/config/passportConfig.js
+++ src/config/passportConfig.js
@@ -7,17 +7,21 @@ import { createHash, isValidPassword } from "../utils/bcrypt.js";
 import userManagerDB from "../dao/utils/userManagerDB.js";
 import CartManagerDB from "../dao/utils/cartManagerDB.js";
 import dotenv from 'dotenv';
+import jwt, { ExtractJwt } from 'passport-jwt';
 
 dotenv.config()
 const userManagerService = new userManagerDB()
 const cartManagerService = new CartManagerDB()
 
 const localStrategy = local.Strategy;
+const JWTStratergy = jwt.Strategy;
+
 const initializatePassport = () => {
   const GHCLIENT_ID = process.env.GHCLIENT_ID
   const GHCLIENT_SECRET = process.env.GHCLIENT_SECRET
   const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
   const GOOGLE_SECRET = process.env.GOOGLE_SECRET
+  const SECRET_KEY_JWT = process.env.SECRET_KEY_JWT
 
   passport.use('register', new localStrategy(
     {
@@ -78,60 +82,72 @@ const initializatePassport = () => {
     }
   ))
 
-  passport.use(
-    'github',
-    new GitHubStrategy({
-      clientID: GHCLIENT_ID,
-      clientSecret: GHCLIENT_SECRET,
-      callbackURL: 'http://localhost:8080/api/session/githubcallback'
-      },
-      async (accessToken, refreshToken, profile, done) => {
-        try {
-            const user = await userModel.findOne({username: profile._json.login})
-            if(!user) {
-                const newUser = {
-                    username: profile._json.login,
-                    name: profile._json.name,
-                    password: ''
-                }
-                const registeredUser = await userManagerService.registerUser(newUser)
-                const cart = await cartManagerService.addCart(registeredUser._id)
-                const result = await userManagerService.updateUser(registeredUser._id, cart._id);
-                done(null, result);
-            } else {
-                done(null, user);
-            }
-        } catch (error) {
-            return done(error);
-        }
+  passport.use('jwt',new JWTStratergy(
+    {
+      jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
+      secretOrKey: 'coderSecret'
+    },
+    async (jwt_payload, done) => {
+      try {
+        return done(null, jwt_payload);
+      } catch (err) {
+        return done(err);
       }
+    }
+  ))
+
+  passport.use('github', new GitHubStrategy(
+    {
+    clientID: GHCLIENT_ID,
+    clientSecret: GHCLIENT_SECRET,
+    callbackURL: 'http://localhost:8080/api/session/githubcallback'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const user = await userModel.findOne({username: profile._json.login})
+        if (!user) {
+          const newUser = {
+            username: profile._json.login,
+            name: profile._json.name,
+            password: ''
+          }
+          const registeredUser = await userManagerService.registerUser(newUser)
+          const cart = await cartManagerService.addCart(registeredUser._id)
+          const result = await userManagerService.updateUser(registeredUser._id, cart._id);
+          done(null, result);
+        } else {
+          done(null, user);
+        }
+      } catch (error) {
+        return done(error);
+      }
+    }
   ));
 
-  passport.use(
-    'google',
-    new GoogleStrategy({
+  passport.use('google', new GoogleStrategy(
+    {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_SECRET,
       callbackURL: 'http://localhost:8080/api/session/googlecallback'
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
-          const user = await userModel.findOne({username: profile._json.name})
-          if(!user) {
-              const newUser = {
-                  username: profile._json.name,
-                  name: profile._json.name,
-                  password: ''
-              }
-              const registeredUser = await userManagerService.registerUser(newUser)
-              const cart = await cartManagerService.addCart(registeredUser._id)
-              const result = await userManagerService.updateUser(registeredUser._id, cart._id);
-              done(null, result);
-          } else {
-              done(null, user);
+        const user = await userModel.findOne({username: profile._json.name})
+        if(!user) {
+          const newUser = {
+            username: profile._json.name,
+            name: profile._json.name,
+            password: ''
           }
+          const registeredUser = await userManagerService.registerUser(newUser)
+          const cart = await cartManagerService.addCart(registeredUser._id)
+          const result = await userManagerService.updateUser(registeredUser._id, cart._id);
+          done(null, result);
+        } else {
+          done(null, user);
+        }
       } catch (error) {
-          return done(error);
+        return done(error);
       }
     })
   )
@@ -142,6 +158,15 @@ const initializatePassport = () => {
       const user = await userModel.findById(id);
       done(null, user);
   })
+}
+
+const cookieExtractor = (req) => {
+  let token = null;
+  if (req && req.cookies) {
+      token = req.cookies.coderCookieToken ?? null;
+  }
+
+  return token;
 }
 
 export default initializatePassport
