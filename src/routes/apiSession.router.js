d62326e7a44a3182b398035793ed568cc9a5e379
JuanPaladea <juanpaladea5@gmail.com>
2024-05-31 17:28:51 -0300

    refactor: Update apiSession.router.js to use userService.getUserById for current user retrieval

--- src/routes/apiSession.router.js
+++ src/routes/apiSession.router.js
@@ -20,7 +20,7 @@ router.get('/users', auth, isAdmin, async (req, res) => {
 
 router.get('/current', auth, async (req, res) => { 
   try {
-    const user = req.session.user;
+    const user = await userService.getUserById(req.session._id);
     res.status(200).send({status: 'success', message: 'User found', user});
   } catch (error) {
     res.status(400).send({status: 'error', message: error.message});
