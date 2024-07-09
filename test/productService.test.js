96db1557d37f6a3ef6222a5c74d3b3daf2675eb1
JuanPaladea <juanpaladea5@gmail.com>
2024-07-08 21:51:31 -0300

    test service

--- test/productService.test.js
+++ test/productService.test.js
@@ -1,53 +1,18 @@
+import * as chai from 'chai';
+import mongoose from "mongoose";
+import { MONGODB_URI } from "../src/utils/config.js";
+const expect = chai.expect;
+
 import productService from "../src/services/productService.js";
 
 describe("productService", () => {
+  before(async () => {
+    await mongoose.connect(MONGODB_URI)
+  });
   describe("getProducts", () => {
     it("should return an array of products", async () => {
-      const limit = 10;
-      const page = 1;
-      const query = {};
-      const sort = { name: 1 };
-
-      const products = await productService.getProducts(limit, page, query, sort);
-
-      expect(Array.isArray(products)).toBe(true);
-      expect(products.length).toBeGreaterThan(0);
-    });
-
-    it("should return products based on the provided query", async () => {
-      const limit = 10;
-      const page = 1;
-      const query = { category: "electronics" };
-      const sort = { name: 1 };
-
-      const products = await productService.getProducts(limit, page, query, sort);
-
-      expect(Array.isArray(products)).toBe(true);
-      expect(products.length).toBeGreaterThan(0);
-
-      products.forEach((product) => {
-        expect(product.category).toBe("electronics");
-      });
-    });
-
-    it("should return products sorted by name in ascending order", async () => {
-      const limit = 10;
-      const page = 1;
-      const query = {};
-      const sort = { name: 1 };
-
-      const products = await productService.getProducts(limit, page, query, sort);
-
-      expect(Array.isArray(products)).toBe(true);
-      expect(products.length).toBeGreaterThan(0);
-
-      let previousName = null;
-      products.forEach((product) => {
-        if (previousName) {
-          expect(product.name).toBeGreaterThan(previousName);
-        }
-        previousName = product.name;
-      });
+      const products = await productService.getProducts(10, 1, null, null);
+      expect(products.products).to.be.an('array');
     });
   });
 });
