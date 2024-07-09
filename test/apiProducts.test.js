4a60817026125f455dc3045c805af30937b52fba
JuanPaladea <juanpaladea5@gmail.com>
2024-07-08 21:18:43 -0300

    cmt2

--- test/apiProducts.test.js
+++ test/apiProducts.test.js
@@ -1,12 +1,10 @@
 import * as chai from 'chai';
-import axios from 'axios';
 
 const expect = chai.expect;
 
 describe('ApiProducts', () => {
   describe('GET /products', () => {
     it('should return a list of products', async () => {
-      const response = await axios.get(`${BASE_URL}/api/products`);
       expect(response.status).to.equal(200);
       expect(response.body.status).to.equal('success');
       expect(response.body.message).to.equal('productos encontrados');
